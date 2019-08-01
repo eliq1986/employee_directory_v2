f1a87ca988109c734cd81f830cb6ff1757330bd8
eliq1986 <elijahmquesada@gmail>
2019-08-01 09:00:10 -0700

    Added checkIfClassMatches func

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-const requestedAmountOfUsers = 12;
 
-const { url , amount, nat } = {
+const { url , amount, nat, cardClassName, cardParentClassName, cardContainerClassName } = {
   url:"https://randomuser.me/api/?",
   amount: "12",
-  nat: "us"
+  nat: "us",
+  cardClassName: ["card-img", "card-name cap","card-text", "card-text cap"],
+  cardParentClassName: ["card-img-container", "card-info-container"],
+  cardContainerClassName: ["card"]
 }
 
 
@@ -39,39 +41,44 @@ function createHtmlCardTemplate({ picture, name, email, location }) {
 
 }
 
-function buildModal({ picture, name, email, location}) {
-
-
-
-  const body = document.querySelector("body");
-  const modal = `
-   <div class="modal-container">
-      <div class="modal">
-          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-          <div class="modal-info-container">
-              <img class="modal-img" src=${picture.large} alt="profile picture">
-              <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
-              <p class="modal-text">${email}</p>
-              <p class="modal-text cap">${location.city}</p>
-              <hr>
-              <p class="modal-text">(555) 555-5555</p>
-              <p class="modal-text">${location.street}, ${location.state} ${location.postcode}</p>
-              <p class="modal-text">Birthday: 10/21/2015</p>
-          </div>
-      </div>
-  `
-  body.insertAdjacentHTML("beforeend", modal);
-  addEventListener();
-
+function checkIfClassMatches(cardClassArr ,cardClassName) {
+ return cardClassArr.filter(possibleClassName => possibleClassName === cardClassName).length;
 }
 
-function addEventListener() {
-  console.log("clicked");
-  document.querySelector("#modal-close-btn").addEventListener("click",()=> {
-    console.log("x was clicked");
-      document.querySelector(".modal-container").style.display = "none";
-  });
-}
+
+//function buildModal({ picture, name, email, location}) {
+
+
+//
+//   const body = document.querySelector("body");
+//   const modal = `
+//    <div class="modal-container">
+//       <div class="modal">
+//           <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+//           <div class="modal-info-container">
+//               <img class="modal-img" src=${picture.large} alt="profile picture">
+//               <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
+//               <p class="modal-text">${email}</p>
+//               <p class="modal-text cap">${location.city}</p>
+//               <hr>
+//               <p class="modal-text">(555) 555-5555</p>
+//               <p class="modal-text">${location.street}, ${location.state} ${location.postcode}</p>
+//               <p class="modal-text">Birthday: 10/21/2015</p>
+//           </div>
+//       </div>
+//   `
+//   body.insertAdjacentHTML("beforeend", modal);
+//   addEventListener();
+//
+// }
+
+// function addEventListener() {
+//   console.log("clicked");
+//   document.querySelector("#modal-close-btn").addEventListener("click",()=> {
+//     console.log("x was clicked");
+//       document.querySelector(".modal-container").style.display = "none";
+//   });
+// }
 
 
 getRandomUsers().then(data => {
@@ -81,19 +88,17 @@ getRandomUsers().then(data => {
    results.forEach( profile => appendCardsToDOM(profile));
 
    document.querySelector("#gallery").addEventListener("click", event=> {
-    if(event.target.className === "card-img" || event.target.className === "card-name cap" || event.target.className === "card-text cap") {
-
+     const elementClassName = event.target.className;
+    if(checkIfClassMatches(cardClassName, elementClassName)) {
       const index = [...document.querySelector("#gallery").children].indexOf(event.target.parentNode.parentNode);
       const cardSelected = results[index];
       buildModal(cardSelected);
-    } else if (event.target.className === "card-info-container" || event.target.className === "card-img-container") {
-      const index = [...document.querySelector("#gallery").children].indexOf(event.target.parentNode);
-      const cardSelected = results[index];
-      buildModal(cardSelected);
-    } else if(event.target.className === "card-img-container" || event.target.className === "card-info-container") {
-      const index = [...document.querySelector("#gallery").children].indexOf(event.target.parentNode);
-      const cardSelected = results[index];
-      buildModal(cardSelected);
+    } else if (checkIfClassMatches(cardParentClassName, elementClassName)) {
+      // const index = [...document.querySelector("#gallery").children].indexOf(event.target.parentNode);
+      // const cardSelected = results[index];
+      // buildModal(cardSelected);
+    } else if(checkIfClassMatches(cardContainerClassName, elementClassName)){
+
     }
    });
 
