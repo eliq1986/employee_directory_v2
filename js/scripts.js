858df4b8519eb145f20ca852e8cbab8e212e2cde
Elijah Quesada <this_boy10@yahoo.com>
2019-08-08 14:31:54 -0700

    Created function appendSearchBarToDOM

--- js/scripts.js
+++ js/scripts.js
@@ -9,23 +9,36 @@ const { url , amount, nat, cardClassName, cardParentClassName, cardContainerClas
 }
 
 
+
 // returns array of objects
 async function getRandomUsers() {
-  const response = await fetch(`${url}results=${amount}&nat=${nat}`);
+  appendSearchBarToDOM();
+   const response = await fetch(`${url}results=${amount}&nat=${nat}`);
    const users = await response.json();
    return users;
+}
+
+
+// takes no arg; appends HTML to DOM.
+function appendSearchBarToDOM() {
+  document.querySelector(".search-container").innerHTML = `
+    <form action="#" method="get">
+        <input type="search" id="search-input" class="search-input" placeholder="Search...">
+        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form>
+  `;
 }
 
 
 // function takes 1 arg obj;
 function appendCardsToDOM(profile) {
   const galleryDiv = document.querySelector("#gallery");
-  const htmlCardTemplate= createHtmlCardTemplate(profile);
+  const htmlCardTemplate= createHTMLCardTemplate(profile);
   galleryDiv.insertAdjacentHTML("beforeend", htmlCardTemplate);
 }
 
 
-function createHtmlCardTemplate({ picture, name, email, location }) {
+function createHTMLCardTemplate({ picture, name, email, location }) {
 
   return `
   <div class="card">
@@ -41,6 +54,33 @@ function createHtmlCardTemplate({ picture, name, email, location }) {
   `
 
 }
+
+function createHTMLModalTemplate({ picture, name, email, location, phone, dob }) {
+  return `
+
+      <div class="modal">
+          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+          <div class="modal-info-container">
+              <img class="modal-img" src=${picture.large} alt="profile picture">
+              <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
+              <p class="modal-text">${email}</p>
+              <p class="modal-text cap">${location.city}</p>
+              <hr>
+              <p class="modal-text">${phone}</p>
+              <p class="modal-text cap">${location.street}, ${location.state} ${location.postcode}</p>
+              <p class="modal-text">Birthday: ${formatBirthday(dob.date)}</p>
+
+              <div class="modal-btn-container">
+                  <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                  <button type="button" id="modal-next" class="modal-next btn">Next</button>
+              </div>
+              </div>
+
+          </div>
+
+  `
+}
+
 
 function checkIfClassMatches(cardClassArr ,cardClassName) {
  return cardClassArr.filter(possibleClassName => possibleClassName === cardClassName).length;
@@ -62,23 +102,25 @@ function formatBirthday(birthdate) {
 function whenCardIsClicked(results) {
 
   document.querySelector("#gallery").addEventListener("click", event => {
-
     const clickedElementClassName = event.target.className;
 
    if(checkIfClassMatches(cardClassName, clickedElementClassName)) {
+     const parent = event.target.parentNode.parentNode;
 
-     const cardSelected = results[getIndexOfCardClicked(event.target.parentNode.parentNode)];
-     buildModal(cardSelected);
+     const cardSelected = results[getIndexOfCardClicked(parent)];
+     buildModal(cardSelected, parent);
 
  } else if (checkIfClassMatches(cardParentClassName, clickedElementClassName)) {
 
       const cardSelected = results[getIndexOfCardClicked(event.target.parentNode)];
-      buildModal(cardSelected);
+      const parent = event.target.parentNode;
+      buildModal(cardSelected, parent);
 
    } else if(checkIfClassMatches(cardContainerClassName, clickedElementClassName)){
 
       const cardSelected = results[getIndexOfCardClicked(event.target)];
-      buildModal(cardSelected);
+      const parent = event.target;
+      buildModal(cardSelected, parent);
    }
   });
 
@@ -86,48 +128,67 @@ function whenCardIsClicked(results) {
 
 
 function tearDownPreviousModal() {
-  const modalContainer = document.querySelector(".modal-container");
+const modalContainer = document.querySelector(".modal-container");
   if(modalContainer) {
     const parent = modalContainer.parentNode;
     parent.removeChild(modalContainer);
   }
 }
 
-function buildModal({ picture, name, email, location, phone, dob }) {
-  tearDownPreviousModal();
-  const body = document.querySelector("body");
+function appendModalToDOM(cardSelected) {
   const div = document.createElement("div");
   div.className = "modal-container";
-  const modal = `
+  div.innerHTML = createHTMLModalTemplate(cardSelected);
+  document.querySelector("body").insertBefore(div, document.querySelector("script"));
+}
 
-      <div class="modal">
-          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-          <div class="modal-info-container">
-              <img class="modal-img" src=${picture.large} alt="profile picture">
-              <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
-              <p class="modal-text">${email}</p>
-              <p class="modal-text cap">${location.city}</p>
-              <hr>
-              <p class="modal-text">${phone}</p>
-              <p class="modal-text cap">${location.street}, ${location.state} ${location.postcode}</p>
-              <p class="modal-text">Birthday: ${formatBirthday(dob.date)}</p>
-          </div>
+function buildModal(cardSelected, parentElement) {
+  tearDownPreviousModal();
+  appendModalToDOM(cardSelected);
+  createModalCloseButton();
 
-  `
-  div.innerHTML = modal;
-  const javaScriptFile = document.querySelector("script");
-  body.insertBefore(div, javaScriptFile);
+  const modalArrows = document.querySelector(".modal-btn-container");
+  modalArrows.addEventListener("click", event => {
+      if(event.target.textContent === "Prev") {
+
+
+      } else if(event.target.textContent === "Next") {
+
+        const nextEmployee = parentElement.nextElementSibling;
+      }
+  });
+
+
+}
+
+function createModalCloseButton() {
   document.querySelector("#modal-close-btn").addEventListener("click", ()=> {
     document.querySelector(".modal-container").style.display = "none";
-
   });
 
 }
 
 
-
 function getIndexOfCardClicked(parentNode) {
   return [...document.querySelector("#gallery").children].indexOf(parentNode);
+}
+
+function getSearchInput() {
+   return searchValue = document.querySelector("#search-input").value.toLowerCase().split(" ");
+}
+
+
+function hideCards(searchInput) {
+
+  const allNames = document.querySelectorAll(".card-name");
+  allNames.forEach(person => {
+  const [firstName, lastName] = person.textContent.split(" ");
+  if(searchInput.indexOf(firstName) === -1 && searchInput.indexOf(lastName) === -1) {
+     const parent = person.parentNode.parentNode;
+     parent.style.display = "none";
+    }
+  });
+
 }
 
 
@@ -139,4 +200,10 @@ getRandomUsers().then(data => {
 
   whenCardIsClicked(results);
 
+});
+
+document.querySelector("#search-submit").addEventListener("click", (e)=> {
+  e.preventDefault();
+  const searchInput = getSearchInput();
+  hideCards(searchInput);
 });
